1161781616e148a350833c7677cc2361e770d571
Rum <215194440@gmail.com>
2025-02-18 15:32:34 +0800

    c

--- src/main/java/com/rum/xianfeng/XianfengApplication.java
+++ src/main/java/com/rum/xianfeng/XianfengApplication.java
@@ -1,13 +1,14 @@
 package com.rum.xianfeng;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.rum.xianfeng.Mapper")
 public class XianfengApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(XianfengApplication.class, args);
     }
-
 }
