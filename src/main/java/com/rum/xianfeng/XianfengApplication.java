afecd675a12e6b957ca9f8f3a077b3e047c100a3
Rum <215194440@gmail.com>
2025-02-18 14:13:21 +0800

    start

---
+++ src/main/java/com/rum/xianfeng/XianfengApplication.java
@@ -0,0 +1,13 @@
+package com.rum.xianfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XianfengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XianfengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rum/xianfeng/XianfengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rum.xianfeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XianfengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
